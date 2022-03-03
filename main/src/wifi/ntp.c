dae69ae0e7104ad73d6e4e90094c49d61b8623f8
823984418 <823984418@qq.com>
2022-03-03 22:54:22 +0800

    修复了 SNTP server 只有一个的bug

--- main/src/wifi/ntp.c
+++ main/src/wifi/ntp.c
@@ -4,15 +4,15 @@
 
 #include "lwip/apps/sntp.h"
 
-void set_ntp_time(const char *const posix_tz, int server_cnt, ...)
+void set_ntp_time(const char *posix_tz, int server_cnt, ...)
 {
     sntp_setoperatingmode(SNTP_OPMODE_POLL);
 	va_list ap;
 	va_start(ap, server_cnt);
-	if (server_cnt > 3)         // sntp_setservername最多支持3个地址
-		server_cnt = 3;         // 超过三个的地址会被抛弃
+	if (server_cnt > SNTP_MAX_SERVERS)         // sntp_setservername最多支持 SNTP_MAX_SERVERS 个地址
+		server_cnt = SNTP_MAX_SERVERS;         // 超过三个的地址会被抛弃
 	for (int i = 0; i < server_cnt; ++i)
-		sntp_setservername(i, va_arg(ap, char *));
+		sntp_setservername(i, va_arg(ap, const char*));
 	va_end(ap);
 	sntp_init();
 	setenv("TZ", posix_tz, 1);
